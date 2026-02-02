96927998e476ed09ac3852cacdf13e666a270eab
Ramakoti Reddy Pandiri <ramky1107@outlook.com>
2026-02-02 23:33:13 +0530

    added live timer

--- FetchDataYFinance.py
+++ FetchDataYFinance.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import constants
+import time
 
 def checkConnectionestablish():
     ticker = yf.Ticker(constants.testCompany)
@@ -7,3 +8,12 @@ def checkConnectionestablish():
     print (data)
 
 checkConnectionestablish()
+
+def getLiveStatus(symbol):
+    ticker = yf.Ticker(symbol)
+    while True:
+        data = ticker.history(interval = "1m", period = "1d")
+        print (data)
+        time.sleep(1000 * 60)
+
+getLiveStatus("AAPL")
