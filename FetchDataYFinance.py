6ba3b2eba99be370a526854a01e5f45f1ffccc5e
RAMAKOTI REDDY <ramky1107@outlook.com>
2026-02-02 17:14:19 +0000

    check apple status

--- FetchDataYFinance.py
+++ FetchDataYFinance.py
@@ -1,1 +1,9 @@
-import
+import yfinance as yf
+import constants
+
+def checkConnectionestablish():
+    ticker = yf.Ticker(constants.testCompany)
+    data = ticker.history(period="1d")
+    print (data)
+
+checkConnectionestablish()
